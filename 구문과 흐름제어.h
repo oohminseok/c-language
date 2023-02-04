470f8cedcef74d64fea193023e1543c693783d94
OHMINSEOK <116791454+oohminseok@users.noreply.github.com>
2023-02-04 22:53:52 +0900

    Add files via upload

---
+++ 구문과 흐름제어.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//인자:키보드,마우스등의 입력을 인자(argumnets)라고 한다.
+//자료구조:데이터를 묶어서 관리하는 것을 자료구조(data structure)라고 한다
+//표현식: 구문
+//Lexicon: 어휘 
+//Syntax: 구문
+//Semantic: 의미 
+//Flow Chart: 순서도
+//전처리기 지시문: #include 컴파일 되기전에 iostream의 내용을 전부 가져와 1번라인에 포함시키며 컴파일 과정에서 가장먼저 변환된다.
